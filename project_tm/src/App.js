5be7571d8ba636a7e3798ae458a2b4d81f05d6e7
Benjamin Piro <brp8396@rit.edu>
2021-09-13 13:50:24 -0400

    Added background image carousel from a 3rd-party repo

--- project_tm/src/App.js
+++ project_tm/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import BackgroundSlider from 'react-background-slider';
+
+import image1 from './assets/bg-1.jpg';
+import image2 from './assets/bg-2.jpg';
+import image3 from './assets/bg-3.jpg';
+import image4 from './assets/bg-4.jpg';
+import image5 from './assets/bg-5.jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div>
+        <div className="App">
+          <header className="App-header">
+          </header>
+        </div>
+  
+        <BackgroundSlider 
+          images={[image1, image2, image3, image4, image5]}
+          duration={8}
+          transition={2}
+        />
+      </div>
+        
+    );
+  }
 
 export default App;
