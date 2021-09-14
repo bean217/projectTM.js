997e4cfa810a75655d46a39c0618cc7d35cb5438
Benjamin Piro <brp8396@rit.edu>
2021-09-14 19:01:09 -0400

    Riot Dev Key Form is now interactive

--- project_tm/src/App.js
+++ project_tm/src/App.js
@@ -6,6 +6,7 @@ import image2 from './assets/bg-2.jpg';
 import image3 from './assets/bg-3.jpg';
 import image4 from './assets/bg-4.jpg';
 import image5 from './assets/bg-5.jpg';
+import image6 from './assets/bg-6.jpg';
 import React from 'react';
 
 class App extends React.Component {
@@ -47,19 +48,50 @@ const FormType = {
 }
 
 class RiotDevKeyForm extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      key: '',
+      errMsg: {
+        isErr: false,
+        msg: "",
+      }
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      key: event.target.value,
+      errMsg: this.state.errMsg,
+    });
+    console.log(this.state.key);
+  }
+
+  handleSubmit(event) {
+    console.log("HANDLE SUBMIT: " + this.state.key);
+    event.preventDefault();
+  }
+
   render() {
     return (<div className="riotdevkeyform">
-      <div class="text">
+      <div className="text">
         Enter Riot Dev Key:
       </div>
-      <div class="input">
-        <input type="text"></input>
+      <div className="input">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.key} onChange={this.handleChange}></input>
+        </form>
       </div>
-      <div class="submit">
-        <button>
+      <div className="submit">
+        <button onClick={this.handleSubmit}>
           Submit
         </button>
       </div>
+      <ErrorMsg display={this.state.errMsg.isErr} errMsg={this.state.errMsg.msg} />
+
       
     </div>);
   }
@@ -78,6 +110,20 @@ class ViewTeamsForm extends React.Component {
     return (<div className="viewteamsform">
       ViewTeamsForm
     </div>);
+  }
+}
+
+class ErrorMsg extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div style={{visibility: this.props.display ? "visible" : "hidden"}} className="errMsg">
+        {this.props.errMsg}
+      </div>
+    );
   }
 }
 
