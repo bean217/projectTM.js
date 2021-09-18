ae11e61004a20c3b19c7756dc41647bf1f28d4a7
Benjamin Piro <brp8396@rit.edu>
2021-09-18 15:50:48 -0400

    Updated the README

---
+++ README.md
@@ -0,0 +1,14 @@
+# PROJECT_TM.js
+
+PROJECT_TM.js is a javascript rewrite of https://github.com/williamztom/ProjectTM using React.js.
+
+The purpose of this project is to give League of Legends tournament maker's an easier time when balancing teams for larger-scale tournaments with 10 or more people.
+
+It is important to note that PROJECT_TM.js only works on RiotGames' NA1 servers.
+
+Feel free to make a pull request to add any features that would advance or improve the functionality of this project!
+
+Author: Benjamin Piro
+
+Added Modules:
+- Made with the help of: https://github.com/u2ix/react-background-slider
--- server/riotApi.js
+++ server/riotApi.js
@@ -4,7 +4,9 @@ const RiotApiURL = 'https://na1.api.riotgames.com';
 
 async function check_api(api_key) {
     try {
-        const apiResult = await axios.get(`${RiotApiURL}/lol/platform/v3/champion-rotations?api_key=${api_key}`);
+        const apiResult = await axios.get(
+            `${RiotApiURL}/lol/platform/v3/champion-rotations?api_key=${api_key}`
+            );
         if (apiResult.status >= 200 && apiResult.status <= 299) {
             return true;
         }
@@ -13,16 +15,6 @@ async function check_api(api_key) {
         // do nothing
         return false;
     }
-
-    // await axios.get(`${RiotApiURL}/lol/platform/v3/champion-rotations?api_key=${api_key}`)
-    //     .then(res => { // got a response (could still be false)
-    //         console.log("STATUS = " + res.status);
-    //         if (res.status == 200) { // accept if response is a 200 status code
-    //             result = true;
-    //         }
-    //     })
-    //     .catch(err => err); // response failed, therefore key may be invalid
-        console.log(`RESULT: ${result}`);
 }
 
 module.exports = { 
