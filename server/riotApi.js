fc01b602bc24e57a1d601c104fb4face8a1c10ca
Benjamin Piro <brp8396@rit.edu>
2021-09-19 19:02:48 -0400

    Added functionality for fetching a player's max rank data

--- server/riotApi.js
+++ server/riotApi.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const utils = require('./utils');
+const get_max_rank = utils.get_max_rank;
 
 const RiotApiURL = 'https://na1.api.riotgames.com';
 
@@ -18,17 +20,43 @@ async function check_api(api_key) {
 }
 
 async function get_summoner(api_key, summonerName) {
-    try {
-        const apiResult = await axios.get(
-            `${RiotApiURL}/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${api_key}`
-        )
-        .then(res => console.log(res.data))
-        .catch(err => console.log(err));
-
-    } catch (err) {
-        console.log("ERROR");
-        // do nothing
+    
+    
+    summonerInfo = {
+        name: summonerName,
     }
+
+    await axios.get(
+        `${RiotApiURL}/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${api_key}`
+        )
+        .then(async res => {
+            console.log(res.data);
+            summonerInfo.id = res.data.id;
+            summonerInfo.level = res.data.summonerLevel;
+            summonerInfo.iconId = res.data.profileIconId;
+
+            await axios.get(
+                `${RiotApiURL}/lol/league/v4/entries/by-summoner/${summonerInfo.id}?api_key=${api_key}`
+                )
+                .then(res => {
+                    console.log(res.data);
+                    summonerInfo.rank = get_max_rank(res.data);
+                })
+                .catch(
+                    err => {
+                        console.log(`${RiotApiURL}/lol/league/v4/entries/by-summoner/${summonerInfo.id}?api_key=${api_key}`);
+                        console.log(`Encountered an error in server/riotApi.js::get_summoner()`);
+                        return { errMsg: `Could not fetch ranked data` };
+                    }
+                );
+        })
+        .catch(err => {
+            // If the player could not be found from the api, return an error
+            console.log(`Encountered an error in server/riotApi.js::get_summoner() - ${err}`);
+            return { errMsg: `'${summonerName}' not found` };
+        });
+
+        console.log(summonerInfo);
 }
 
 module.exports = { 
---
+++ server/utils.js
@@ -0,0 +1,44 @@
+const TIER = {
+    'IRON': 1,
+    'BRONZE': 2,
+    'SILVER': 3,
+    'GOLD': 4,
+    'PLATINUM': 5,
+    'DIAMOND': 6,
+    'MASTER': 7,
+    'GRANDMASTER': 8,
+    'CHALLENGER': 9,
+}
+
+const RANK = {
+    'IV': 1,
+    'III': 2,
+    'II': 3,
+    'I': 4,
+}
+
+function get_max_rank(rank_data) {
+    if (rank_data.length === 0) return null;
+
+    const data = rank_data.map(item => [item.tier, item.rank, item.leaguePoints]);
+    const vals = data.map(item => get_rank_value(item[0], item[1]));
+
+    let max = 0;
+    for (let i = 1; i < vals.length; i++) {
+        if (vals[max] < vals[i]) max = i;
+    }
+
+    return {
+        tier: data[max][0],
+        rank: data[max][1],
+        LP: data[max][2], 
+    };
+}
+
+function get_rank_value(tier, rank) {
+    return RANK[rank] + TIER[tier] * 10;
+}
+
+module.exports = {
+    get_max_rank: get_max_rank,
+};
