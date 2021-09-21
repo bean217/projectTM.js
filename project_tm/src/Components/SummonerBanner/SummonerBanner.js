9e2e5f0d206e27b502b381f8d3189b03fcd34b7a
Benjamin Piro <brp8396@rit.edu>
2021-09-21 17:41:00 -0400

    Clicking on an added summoner displays a banner with their player data

--- project_tm/src/Components/SummonerBanner/SummonerBanner.js
+++ project_tm/src/Components/SummonerBanner/SummonerBanner.js
@@ -5,10 +5,29 @@ export class SummonerBanner extends React.Component {
     render() {
         return (
             <div className="summonerbanner">
-                <img 
-                    src={`http://ddragon.leagueoflegends.com/cdn/${this.props.ddrag_version}/img/profileicon/${this.props.player.iconId}.png`}
-                    alt="Missing Icon">
-                </img>
+                <div className="icon">
+                    <img 
+                        src={`http://ddragon.leagueoflegends.com/cdn/${this.props.ddrag_version}/img/profileicon/${this.props.player.iconId}.png`}
+                        alt="Missing Icon">
+                    </img>
+                </div>
+                <div className="playerdata">
+                    <div id="name">{this.props.player.name}</div>
+                    <div id="level">{this.props.player.level}</div>
+                    {this.props.player.rank !== null ? 
+                        <div id="rank">
+                            {`${this.props.player.rank.tier} ${this.props.player.rank.rank}`}
+                        </div> : ""}
+
+                        {this.props.player.rank !== null ? 
+                        (<div id="LP">
+                            {`${this.props.player.rank.LP} LP`}
+                        </div>) : ""}
+                </div>
+                <div className="roledata">
+                    <div id="role1">{`Primary: ${this.props.player.role1}`}</div>
+                    <div id="role2">{`Secondary: ${this.props.player.role2}`}</div>
+                </div>
             </div>
         );
     }
